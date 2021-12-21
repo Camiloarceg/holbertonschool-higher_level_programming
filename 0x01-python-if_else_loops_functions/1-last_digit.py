45c2c4f9d7557a6ff2ee2c9866b8b95ffa52bf15
Juan Camilo Arce <3773@holbertonschool.com>
2021-12-21 13:07:06 -0500

    fixed task checker

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -9,9 +9,9 @@ else:
     ld = int(ld)
 if ld > 5:
     print('Last digit of {:d} is {:d}\
-            and is greater than 5\n'.format(number, ld))
+            and is greater than 5'.format(number, ld))
 elif lastdigit == 0:
-    print('Last digit of {:d} is {:d} and is 0\n'.format(number, ld))
+    print('Last digit of {:d} is {:d} and is 0'.format(number, ld))
 else:
     print('Last digit of {:d} is {:d}\
-            and is less than 6 and not 0\n'.format(number, ld))
+            and is less than 6 and not 0'.format(number, ld))
