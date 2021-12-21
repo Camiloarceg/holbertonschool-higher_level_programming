78bc74ec7da47d9df2d42cd6a2008804d05650c7
Juan Camilo Arce <3773@holbertonschool.com>
2021-12-21 13:14:58 -0500

    fixed task

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-for i in range(97, 123):
-    if i != 101 and i != 113:
-        print("{}".format(chr(i)), end="")
+for i in range(ord('a'), ord('z') + 1):
+    if i == ord('q') or i == ord('e'):
+        continue
+    print("{:c}".format(i), end="")
