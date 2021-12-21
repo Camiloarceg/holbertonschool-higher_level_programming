074fd37cb08ba709036e8269ffe9a5064b0a3baa
Juan Camilo Arce <3773@holbertonschool.com>
2021-12-21 10:30:03 -0500

    add task 1 prechecker

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+ld = repr(number)[-1]
+if number < 0:
+    ld = '-' + ld
+    ld = int(ld)
+else:
+    ld = int(ld)
+if ld > 5:
+    print('Last digit of {:d} is {:d}\
+            and is greater than 5\n'.format(number, ld))
+elif lastdigit == 0:
+    print('Last digit of {:d} is {:d} and is 0\n'.format(number, ld))
+else:
+    print('Last digit of {:d} is {:d}\
+            and is less than 6 and not 0\n'.format(number, ld))
