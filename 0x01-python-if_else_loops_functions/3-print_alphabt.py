77ce2ac6f0a6626128e0434161df4012e23631e0
Juan Camilo Arce <3773@holbertonschool.com>
2021-12-21 12:59:15 -0500

    pycodestyle corrected

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 for i in range(ord('a'), ord('z')+1):
-    if i in [101,113]:
+    if i in [101, 113]:
         continue
-    print("{:c}".format(i), end ='')
+    print("{:c}".format(i), end='')
