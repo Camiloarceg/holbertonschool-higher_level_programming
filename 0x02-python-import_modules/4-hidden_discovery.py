9b4b1d69fb4f966b2ef094277e51cf1258acc727
Juan Camilo Arce <3773@holbertonschool.com>
2021-12-23 14:30:25 -0500

    add task 4

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -3,12 +3,10 @@
 import hidden_4
 
 
-def run():
-
-    o_hidden_4 = dir()
-    for i in range(0, len(o_hidden_4)):
-        if not o_hidden_4[i][0:2] == "__":
-            print("{}".format(o_hidden_4[i]))
+def main():
+    for i in dir(hidden_4):
+        if not i.startswith("__"):
+            print(i)
 
 if __name__ == "__main__":
-    run()
+    main()
