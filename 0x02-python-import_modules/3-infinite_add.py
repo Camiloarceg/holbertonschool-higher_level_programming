862b17e2f3bb3077f4e9cefd5816d02ee46a903a
Juan Camilo Arce <3773@holbertonschool.com>
2021-12-23 13:39:21 -0500

    add task 3

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+from sys import argv
+
+def main():
+    add = 0
+    length = len(argv) - 1
+    if length == 0:
+        print('{}'.format(add))
+    else:
+        for i in range(1, length + 1):
+            add += int(argv[i])
+        print('{}'.format(add))
+
+
+if __name__ == '__main__':
+    main()
