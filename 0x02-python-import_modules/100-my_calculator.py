69f3fca848be292c6b2f7c23772d5fbedafc4704
Juan Camilo Arce <3773@holbertonschool.com>
2021-12-23 15:55:50 -0500

    add task 6

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 
 from calculator_1 import add, sub, mul, div
-
 from sys import argv
 
 
@@ -15,12 +14,11 @@ def main():
     operators = ['+', '-', '*', '/']
     functions = [add(int(a), int(b)), sub(int(a),
                  int(b)), mul(int(a), int(b)), div(int(a), int(b))]
-
     for i in range(len(operators)):
         if operator == operators[i]:
             result = functions[i]
             print('{} {} {} = {}'.format(a, operator, b, result))
-            break
+            exit(0)
 
     print('Unknown operator. Available operators: +, -, * and /')
     exit(1)
