a3b8931f6ed6cc732427092ba3ee35ee14b26df6
Juan Camilo Arce <3773@holbertonschool.com>
2021-12-23 11:49:57 -0500

    fixed code style task 0 and 1, add task 2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+from sys import argv
+
+
+def main():
+
+    length = len(argv) - 1
+    if length == 0:
+        print('{} arguments.'.format(length))
+    elif length == 1:
+        print('{} argument:'.format(length))
+        print('{}: {}'.format(length, argv[0]))
+    else:
+        print('{} arguments:'.format(length))
+        for i in range(1, length + 1):
+            print('{}: {}'.format(i, argv[i]))
+
+
+if __name__ == "__main__":
+    main()
