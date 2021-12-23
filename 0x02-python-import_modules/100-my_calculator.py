b8311c5940049e62cf1a4ac376e4fb6db877ec3c
Juan Camilo Arce <3773@holbertonschool.com>
2021-12-23 15:21:51 -0500

    add task 6

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+from calculator_1 import add, sub, mul, div
+
+from sys import argv
+
+
+def main():
+    length = len(argv) - 1
+    if length != 3:
+        print('Usage: ./100-my_calculator.py <a> <operator> <b>')
+        exit(1)
+    a = argv[1]
+    operator = argv[2]
+    b = argv[3]
+    operators = ['+', '-', '*', '/']
+    functions = [add(int(a), int(b)), sub(int(a), int(b)), mul(int(a), int(b)), div(int(a), int(b))]
+
+    for i in range(len(operators)):
+        if operator == operators[i]:
+            result = functions[i]
+            print('{} {} {} = {}'.format(a, operator, b, result))
+            break
+        else:
+            print('Unknown operator. Available operators: +, -, * and /')
+            exit(1)
+
+
+if __name__ == "__main__":
+    main()
