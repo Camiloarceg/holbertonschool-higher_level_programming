697e0e6f1aacc244ec45dc2ff07d34a7792dbba3
Juan_Camilo_Arce <camiloarceg@gmail.com>
2022-01-03 22:55:37 +0000

    add task 8

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -5,5 +5,5 @@ def multiple_returns(sentence):
     if sentence:
         the_tuple = (len(sentence), sentence[0])
     else:
-        the_tuple = (0, 'None')
-    return te_tuple
+        the_tuple = (0, None)
+    return the_tuple
