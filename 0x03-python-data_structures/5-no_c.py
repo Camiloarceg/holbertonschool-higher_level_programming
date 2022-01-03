2c0fd848a012b9a4e5c48eb4422f3b9a399a88c2
Juan_Camilo_Arce <camiloarceg@gmail.com>
2022-01-03 21:42:59 +0000

    add task 5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,6 +2,8 @@
 
 
 def no_c(my_string):
+    new_string = ''
     for i in my_string:
-        if i == 'c' or 'C':
-            i = ''
+        if i != 'c' and i != 'C':
+            new_string += i 
+    return new_string
