b4ddae8bb8e218e3c39e754504347f29cfec2a78
Juan_Camilo_Arce <camiloarceg@gmail.com>
2022-01-03 23:02:38 +0000

    add task 9

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -5,7 +5,7 @@ def max_integer(my_list=[]):
     max_num = 0
     if my_list:
         for num in my_list:
-            if num > max_num:
+            if num >= max_num:
                 max_num = num
     else:
         return None
