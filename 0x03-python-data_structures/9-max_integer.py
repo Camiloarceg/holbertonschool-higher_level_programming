674e51ce551af8b6a23e4d2648ec141172b0a2d5
Juan_Camilo_Arce <camiloarceg@gmail.com>
2022-01-03 23:00:30 +0000

    add task 9

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+
+def max_integer(my_list=[]):
+    max_num = 0
+    if my_list:
+        for num in my_list:
+            if num > max_num:
+                max_num = num
+    else:
+        return None
+    return max_num
