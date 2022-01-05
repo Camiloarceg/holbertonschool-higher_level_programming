666ae062566330880ae0320088461172cb6a2937
Juan_Camilo_Arce <camiloarceg@gmail.com>
2022-01-05 21:56:56 +0000

    10-best_score.py

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -6,7 +6,7 @@ def best_score(a_dictionary):
         keys = list(a_dictionary)
         max_val = a_dictionary[keys[0]]
         for key in keys:
-            if a_dictionary[key] >= max_val:
+            if a_dictionary[key] > max_val:
                 max_key = key
                 return max_key
     else:
