7018ae60f77803f49a2dc0fbd980dd8150e2f9ec
Camiloarceg <camiloarceg@gmail.com>
2022-01-17 13:38:35 -0500

    add task 7

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+
+import sys
+
+
+def safe_print_integer_err(value):
+    try:
+        print("{:d}".format(value))
+        return True
+
+    except (ValueError, TypeError) as val_e:
+        print("Exception: {}".format(val_e), file=sys.stderr)
+        return False
