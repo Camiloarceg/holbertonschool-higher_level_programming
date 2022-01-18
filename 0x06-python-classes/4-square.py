230552536c4db87d5fff0dc0f44706d893e8d155
Camiloarceg <camiloarceg@gmail.com>
2022-01-18 17:42:14 -0500

    add task 4

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,43 @@
+class Square:
+    """Square class with an constructor"""
+    def __init__(self, size=0):
+        """constructor for the class Square
+
+        Args:
+            size (int): The size of a square
+        """
+        if not isinstance(size, int):
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = size
+
+    @property
+    def size(self):
+        """getter for size attribute
+
+        Returns:
+            [int]: size of the square
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """setter for size attribute
+
+        Args:
+            value (int): size of the square
+        """
+        if not isinstance(value, int):
+            raise TypeError('size must be an integer')
+        if value < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = value
+
+    def area(self):
+        """returns the current square area
+
+        Returns:
+            [int]: Area of the Square computed
+        """
+        return self.size * self.size
