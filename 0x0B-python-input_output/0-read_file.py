262f1adeb7cd1ee40fd6bb1a8f12fea566896341
Camiloarceg <camiloarceg@gmail.com>
2022-02-01 18:51:13 -0500

    add

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -6,4 +6,4 @@ def read_file(filename=""):
     """ function that reads files """
 
     with open(filename, encoding="utf-8") as myFile:
-        print(myFile.read())
+        print(myFile.read(), end="")
