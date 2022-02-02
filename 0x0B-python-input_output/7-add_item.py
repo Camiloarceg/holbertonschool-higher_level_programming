cf18e03a79fbbd81407d3dde4dc000c18f5f25d2
Camiloarceg <camiloarceg@gmail.com>
2022-02-01 19:39:49 -0500

    add

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -6,7 +6,7 @@ load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 
 try:
-    content = load_from_json_file("add_item.json")
+    listing = load_from_json_file("add_item.json")
 except FileNotFoundError:
-    content = []
-save_to_json_file(content + sys.argv[1:], "add_item.json")
+    listing = []
+save_to_json_file(listing + sys.argv[1:], "add_item.json")
