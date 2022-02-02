37026d3072a4116a09a20a6922dd5036a8e04270
Juan Camilo Arce <3773@holbertonschool.com>
2022-02-02 12:09:05 -0500

    add task 9

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -14,8 +14,11 @@ def pascal_triangle(n):
         print(list, end="")
         print()
 
+
 def factorial(n):
-    return 1 if (n==1 or n==0) else n * factorial(n - 1)
+
+    return 1 if (n == 1 or n == 0) else n * factorial(n - 1)
+
 
 if __name__ == "__main__":
     pascal_triangle(5)
