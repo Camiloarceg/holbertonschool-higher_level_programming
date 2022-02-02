7b92550d7f2224cc702a4569b914a86e959e1f58
Camiloarceg <camiloarceg@gmail.com>
2022-02-01 19:48:11 -0500

    add

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+Function returns dictionary description with simple data structure (list,
+dictionary, string, integer, boolean) for JSON serialization of an object
+"""
+
+
+def class_to_json(obj):
+    """ Dict representation of a class """
+    return obj.__dict__
