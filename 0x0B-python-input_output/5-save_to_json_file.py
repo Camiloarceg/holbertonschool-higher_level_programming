83b441da1cdf0460b07db758db1d4b05cf115b38
Camiloarceg <camiloarceg@gmail.com>
2022-02-01 19:07:38 -0500

    add

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" A module with only one function """
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """ a function that writes an Object to a text file,
+        using a JSON representation
+    """
+
+    with open(filename, mode="w", encoding="utf-8") as myFile:
+           return json.dump(my_obj, myFile)
