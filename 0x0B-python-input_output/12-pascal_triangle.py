487171733aeb3186c7132ffa4d72c66bf0ec8326
Juan Camilo Arce <3773@holbertonschool.com>
2022-02-02 12:06:08 -0500

    add task 9

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" This module contains a triangle pascal """
+
+
+def pascal_triangle(n):
+    """ asfsfasgdsgdgs efsgwsegs """
+
+    list = []
+    for nivel in range(n):
+        list = []
+        for j in range(nivel, -1, -1):
+            valor = factorial(nivel)/(factorial(j)*factorial(nivel-j))
+            list.append(int(valor))
+        print(list, end="")
+        print()
+
+def factorial(n):
+    return 1 if (n==1 or n==0) else n * factorial(n - 1)
+
+if __name__ == "__main__":
+    pascal_triangle(5)
