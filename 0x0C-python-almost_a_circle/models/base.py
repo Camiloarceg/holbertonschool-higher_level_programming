d43035164c7b52a437c0f4f6359d2e8967d8916e
Camiloarceg <camiloarceg@gmail.com>
2022-02-07 19:18:24 -0500

    add

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -7,8 +7,10 @@ class Base:
     """ Base class for the project almost a circle """
 
     __nb_objects = 0
+
     def __init__(self, id=None):
         """ The Class constructor for Base class """
+
         if id is not None:
             self.id = id
         else:
