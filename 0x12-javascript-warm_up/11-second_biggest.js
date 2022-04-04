a44f8fd5216ef8497093bda67e447269b6955eec
Camiloarceg <camiloarceg@gmail.com>
2022-04-04 18:28:48 -0500

    add task 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,11 +1,12 @@
 #!/usr/bin/node
 let list = process.argv;
+const array = [];
 list = list.slice(2, list.length);
 if (list.length > 1) {
-  for (let i = 0; i < list; i++) {
-    list[i] = parseInt(list[i]);
+  for (let i = 0; i < list.length; i++) {
+    array.push(parseInt(list[i]));
   }
-  const sortedList = list.sort((a, b) => a > b ? -1 : 1);
+  const sortedList = array.sort((a, b) => a > b ? -1 : 1);
   console.log(sortedList[1]);
 } else {
   console.log(0);
