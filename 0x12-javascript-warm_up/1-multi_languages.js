45c722f8b441dbb32ae2419ba458f687b55eb5f2
Camiloarceg <camiloarceg@gmail.com>
2022-04-04 15:43:09 -0500

    add task 1

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+const myLangs = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (let index = 0; index < myLangs.length; index++) {
+  const element = myLangs[index];
+  console.log(element);
+}
