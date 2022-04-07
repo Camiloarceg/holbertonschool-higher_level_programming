b380a5f14a4616865712b4007735ea8f46c11bff
Camiloarceg <camiloarceg@gmail.com>
2022-04-07 06:36:55 -0500

    hot fix

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,11 +1,10 @@
 #!/usr/bin/node
 const oldDict = require('./101-data').dict;
 const newDict = {};
-let list = []
-for (const [key, value] of Object.entries(oldDict)) {
-    newDict[value] = []
+for (const value of Object.values(oldDict)) {
+  newDict[value] = [];
 }
 for (const [key, value] of Object.entries(oldDict)) {
-    newDict[value].push(key);
+  newDict[value].push(key);
 }
 console.log(newDict);
