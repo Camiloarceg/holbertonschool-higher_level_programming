101a8054f9b8450f0a2f6d21bc40d51a3b6d2521
Camiloarceg <camiloarceg@gmail.com>
2022-05-10 08:38:03 -0500

    add

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,17 +1,12 @@
 #!/usr/bin/python3
-import sys
-import MySQLdb
 """ takes in an argument and displays all values in the states table
     of hbtn_0e_0_usa where name matches the argument.
 """
 if __name__ == "__main__":
-    mydb = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=sys.argv[1],
-        password=sys.argv[2],
-        database=sys.argv[3]
-    )
+    import sys
+    import MySQLdb
+    mydb = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                           password=sys.argv[2], database=sys.argv[3])
     mycursor = mydb.cursor()
     q = "SELECT * FROM states\
         WHERE name LIKE BINARY '{}' ORDER BY id ASC".format(sys.argv[4])
