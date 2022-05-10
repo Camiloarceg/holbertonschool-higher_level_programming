93f370f1cc1079294eeebe5f5c3ff3ec233a416e
Camiloarceg <camiloarceg@gmail.com>
2022-05-10 15:13:44 -0500

    add nothing case

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -20,4 +20,7 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
     result = session.query(State).first()
-    print("{}: {}".format(result.id, result.name))
+    if result:
+        print("{}: {}".format(result.id, result.name))
+    else:
+        print("Nothing")
