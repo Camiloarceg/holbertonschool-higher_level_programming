ac87b4fdd8f4be094196ff4692cbc37f39f34b01
Camiloarceg <camiloarceg@gmail.com>
2022-05-11 13:51:22 -0500

    add task 16

---
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""The second adv task list all city of all states
+"""
+import sys
+from relationship_state import Base, State
+from relationship_city import City
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    db_parameters = {
+            "host": "localhost",
+            "port": 3306,
+            "user": sys.argv[1],
+            "pass": sys.argv[2],
+            "db": sys.argv[3]
+            }
+    engine = create_engine('mysql+mysqldb://{user}:{pass}@{host}:{port}/{db}'
+                           .format(**db_parameters), pool_pre_ping=True)
+
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+for state in session.query(State).all():
+    print(f"{state.id}: {state.name}")
+    for city in state.cities:
+        print(f"    {city.id}: {city.name}")
