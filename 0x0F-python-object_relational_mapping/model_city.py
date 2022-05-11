c64cbe30b9722c2c5d7fc523a7a5015e20e1c349
Camiloarceg <camiloarceg@gmail.com>
2022-05-10 19:57:29 -0500

    add tas

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -12,6 +12,6 @@ class City(Base):
     """ City class inherits from Base
     """
     __tablename__ = 'cities'
-    id = Column(Integer, primary_key=True,autoincrement=True, unique=True, nullable=False)
+    id = Column(Integer, primary_key=True, unique=True, nullable=False)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
