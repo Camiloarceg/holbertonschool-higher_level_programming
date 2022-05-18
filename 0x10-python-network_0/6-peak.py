8df6ee8c90d4261e76c2dd8736efdc22346db941
Camiloarceg <camiloarceg@gmail.com>
2022-05-18 18:12:35 -0500

    fix pycode

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """  finds a peak in a list of unsorted integers. """
+
+
 def find_peak(list_of_integers):
     " Sort a list and return the last number of it. "
     if list_of_integers:
