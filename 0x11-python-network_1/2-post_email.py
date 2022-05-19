025040f50c62556710c381d764022b41be1783bc
Camiloarceg <camiloarceg@gmail.com>
2022-05-19 18:43:46 -0500

    add

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" Write a Python script that fetches """
+import urllib.request
+from sys import argv
+
+if __name__ == "__main__":
+    data = urllib.parse.urlencode({'email': argv[2]})
+    data = data.encode('utf-8')
+    with urllib.request.urlopen(argv[1], data) as response:
+        html = response.read()
+    html = html.decode('utf-8')
+    print(html)
