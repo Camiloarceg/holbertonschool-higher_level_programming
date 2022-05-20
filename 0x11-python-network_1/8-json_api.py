9c454536624f79ff788ed9e9ed4d9a09a9508fc2
Camiloarceg <camiloarceg@gmail.com>
2022-05-19 20:43:45 -0500

    add

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" script that fetches https://intranet.hbtn.io/status """
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    url = 'http://0.0.0.0:5000/search_user'
+    values = {'q': ""}
+    if len(argv) == 2:
+        values['q'] = argv[1]
+        response = requests.post(url, data=values)
+    else:
+        response = requests.post(url, data=values)
+    try:
+        user = response.json()
+        if len(user) < 1:
+            print("No result")
+        else:
+            print("[{}] {}".format(user["id"], user["name"]))
+    except ValueError:
+        print("Not a valid JSON")
